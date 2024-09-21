8af4a5620f7775f86628d53c6610983b698f6785
connor <connor.alexander89@outlook.com>
2024-09-20 23:43:23 -0500

    update

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,15 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+export default {
+    kit: {
+        adapter: adapter({
+            // default options are shown
+            pages: 'build',
+            assets: 'build',
+            fallback: '200.html'
+        }),
+        paths: {
+            base: process.env.NODE_ENV === 'production' ? '/attorney-connect' : ''
+        }
+    }
 };
-
-export default config;
