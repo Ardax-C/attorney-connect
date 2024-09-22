930cedb67594e76669b1cc58469056dedbeff8a1
connor <connor.alexander89@outlook.com>
2024-09-21 21:18:49 -0500

    update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,10 @@
     import '../app.css'
 
     import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+    import { inject } from '@vercel/analytics'
 
     injectSpeedInsights();
+    inject();
 </script>
 
 <slot />
