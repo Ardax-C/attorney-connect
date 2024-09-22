36ab889b1d4586c88be1ac308c41a2938d7b1136
connor <connor.alexander89@outlook.com>
2024-09-21 20:05:25 -0500

    update

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,9 @@
-import vercel from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-vercel';
 
-export default {
-  kit: {
-    adapter: vercel()
-  }
+const config = {
+    kit: {
+        adapter: adapter()
+    }
 };
+
+export default config;
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,18 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
+import * as env from 'dotenv';
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  return {
-    plugins: [sveltekit()],
-    define: {
-      'process.env.PUBLIC_FIREBASE_API_KEY': JSON.stringify(env.PUBLIC_FIREBASE_API_KEY),
-      'process.env.PUBLIC_FIREBASE_AUTH_DOMAIN': JSON.stringify(env.PUBLIC_FIREBASE_AUTH_DOMAIN),
-      'process.env.PUBLIC_FIREBASE_PROJECT_ID': JSON.stringify(env.PUBLIC_FIREBASE_PROJECT_ID),
-      'process.env.PUBLIC_FIREBASE_STORAGE_BUCKET': JSON.stringify(env.PUBLIC_FIREBASE_STORAGE_BUCKET),
-      'process.env.PUBLIC_FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(env.PUBLIC_FIREBASE_MESSAGING_SENDER_ID),
-      'process.env.PUBLIC_FIREBASE_APP_ID': JSON.stringify(env.PUBLIC_FIREBASE_APP_ID),
-      'process.env.PUBLIC_FIREBASE_MEASUREMENT_ID': JSON.stringify(env.PUBLIC_FIREBASE_MEASUREMENT_ID),
-    }
-  };
+env.config();
+
+export default defineConfig({
+  plugins: [sveltekit()],
+  define: {
+    'process.env.PUBLIC_FIREBASE_API_KEY': JSON.stringify(process.env.PUBLIC_FIREBASE_API_KEY),
+    'process.env.PUBLIC_FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.PUBLIC_FIREBASE_AUTH_DOMAIN),
+    'process.env.PUBLIC_FIREBASE_PROJECT_ID': JSON.stringify(process.env.PUBLIC_FIREBASE_PROJECT_ID),
+    'process.env.PUBLIC_FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.PUBLIC_FIREBASE_STORAGE_BUCKET),
+    'process.env.PUBLIC_FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.PUBLIC_FIREBASE_MESSAGING_SENDER_ID),
+    'process.env.PUBLIC_FIREBASE_APP_ID': JSON.stringify(process.env.PUBLIC_FIREBASE_APP_ID),
+    'process.env.PUBLIC_FIREBASE_MEASUREMENT_ID': JSON.stringify(process.env.PUBLIC_FIREBASE_MEASUREMENT_ID),
+  }
 });
