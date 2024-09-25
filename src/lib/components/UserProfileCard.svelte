d3634708f2e2cce7b2bc64346ea077fe4b7bca28
connor <connor.alexander89@outlook.com>
2024-09-24 20:51:58 -0500

    update css

--- src/lib/components/UserProfileCard.svelte
+++ src/lib/components/UserProfileCard.svelte
@@ -8,7 +8,7 @@
     }
 </script>
 
-<div class="bg-zinc-700 p-4 rounded-sm w-full h-full flex flex-col overflow-hidden">
+<div class="bg-zinc-700 p-4 rounded-sm w-full h-full flex flex-col overflow-hidden hover:bg-slate-900 hover:border hover:border-yellow-700 hover:shadow-lg hover:shadow-slate-900">
     <div class="flex items-center justify-center mb-3">
         <img src={user.profilePictureUrl || '/default-profile.png'} alt={`${user.firstName} ${user.lastName}`} class="w-24 h-24 rounded-full object-cover" />
     </div>
