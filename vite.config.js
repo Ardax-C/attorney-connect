991ca5c6925671f60401784c3275c34251da8a72
connor <connor.alexander89@outlook.com>
2024-10-11 20:01:49 -0500

    update config

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,15 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [sveltekit()],
+  optimizeDeps: {
+    include: ['fuse.js']
+  },
+  build: {
+    commonjsOptions: {
+      include: [/node_modules/]
+    }
+  },
+  ssr: {
+    noExternal: ['fuse.js']
+  }
 });
